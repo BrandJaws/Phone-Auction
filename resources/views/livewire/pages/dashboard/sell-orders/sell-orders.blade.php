acca0b69a529db4003ca21264be3ea5e8442c8a3
Muhammad Salman Abid <salman3k1@gmail.com>
2020-12-07 20:36:53 +0500

    Fix caption for order type

--- resources/views/livewire/pages/dashboard/sell-orders/sell-orders.blade.php
+++ resources/views/livewire/pages/dashboard/sell-orders/sell-orders.blade.php
@@ -19,7 +19,7 @@
                 <tr>
                     <td class="border px-4 py-2">{{ !$sellOrder->selfDropToLocation ? $sellOrder->firstName.' '.$sellOrder->lastName : '-' }} </td>
                     <td class="border px-4 py-2">{{ $sellOrder->paymentEmail }} </td>
-                    <td class="border px-4 py-2">{{ $sellOrder->selfDropToLocation ? 'Mail' : 'Drop At Store' }} </td>
+                    <td class="border px-4 py-2">{{ $sellOrder->selfDropToLocation ? 'Drop At Store' : 'Mail' }} </td>
                     <td class="border px-4 py-2 text-right">
                         <a class="text-sm bg-white btnSmall hover:bg-white-700 text-black border border-gray-400 font-bold py-2 rounded" href="{{route('dashboard.sell-orders.view', $sellOrder->id)}}"> View </a>
                         <a class="text-sm bg-red-600 btnSmall hover:bg-red-700 text-white font-bold py-2 rounded" href="#." wire:click.prevent="delete({{$sellOrder->id}})" > Delete </a>
