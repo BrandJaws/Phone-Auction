0475c5a4a66b1634bfce9600d6fb7a56113ecb3e
hassan-siraj <hassanikram84@gmail.com>
2021-03-19 18:27:13 +0500

    responsive setting

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/all.css?version=1.2') }}">
+        <link rel="stylesheet" href="{{ asset('css/all.css?version=1.3') }}">
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
